e80858e66ab70ba3689b0b8a03432c91add4ba51
Marian-13 <marian144519@gmail.com>
2016-10-24 21:29:51 +0300

    Static tweets

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,17 @@ class HomeController < ApplicationController
   before_action :authorize
 
   def index
+    @client = Twitter::REST::Client.new do |config|
+      config.consumer_key        = ENV['CONSUMER_KEY']
+      config.consumer_secret     = ENV['CONSUMER_SECRET']
+      config.access_token        = ENV['ACCESS_TOKEN']
+      config.access_token_secret = ENV['ACCESS_TOKEN_SECRET']
+    end
+
+    begin
+      @tweets = @client.user_timeline('Lamborghini', count: 50)
+    rescue Twitter::Error
+      render 'TODO Rescue Twitter::Error'
+    end
   end
 end
