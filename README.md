9bc08c5ba22a33d61fb3b7e67bc4b94dec366585
Marian-13 <marian144519@gmail.com>
2016-10-26 16:38:02 +0300

    More than 45 unique tweets

--- README.md
+++ README.md
@@ -8,4 +8,7 @@
 6. Ajax for new tweets
 7. Job for tweets processing
 8. Application footer
-9. 
+9. Index database
+10. Current user in class variable?
+11. Right way to track search query modification
+12. Tweets by tag
--- lib/twitter_utils/twitter_client.rb
+++ lib/twitter_utils/twitter_client.rb
@@ -15,12 +15,43 @@ module TwitterUtils
 
       @tweets_with_pictures = []
       @tweets_with_pictures_remaining = 0
+      @last_new_tweet_id = nil
+      @search_query = nil
     end
 
-    def fetch_tweets_with_pictures(user_name, count: 5)
-      if @tweets_with_pictures_remaining < count
-        new_tweets = @client.user_timeline(user_name, count: MAXIMUM_SEARCH_RESULTS)
+    def fetch_tweets_with_pictures(search_query, search_type=nil, count: 5)
+      # TODO Right way to track search query modification
+      if @search_query != search_query
+        @search_query = search_query
+        first_fetching(@search_query)
+      end
 
+      if @tweets_with_pictures_remaining < count
+        # max_id is inclusive
+        # TODO
+        # new_tweets = @client.user_timeline(search_query, count: MAXIMUM_SEARCH_RESULTS,
+        #                                                  max_id: @last_new_tweet_id - 1)
+        new_tweets = fetch_new_tweets(@search_query)
+
+        select_tweets_with_pictures_from!(new_tweets)
+        @last_new_tweet_id = new_tweets.last.id
+      end
+
+      @tweets_with_pictures_remaining -= 5
+
+      # Deletes the first {count} elements and returns array of them (... in range)
+      @tweets_with_pictures.slice!(0...count)
+    end
+
+    private
+      def first_fetching(search_query)
+        new_tweets = @client.user_timeline(search_query, count: MAXIMUM_SEARCH_RESULTS)
+        select_tweets_with_pictures_from!(new_tweets)
+        @last_new_tweet_id = new_tweets.last.id
+      end
+
+      # TODO refactor
+      def select_tweets_with_pictures_from!(new_tweets)
         new_tweets.each do |tweet|
           unless tweet.media.blank?
             @tweets_with_pictures_remaining += 1
@@ -29,10 +60,25 @@ module TwitterUtils
         end
       end
 
-      @tweets_with_pictures_remaining -= 5
+      # TODO
+      def fetch_new_tweets(search_query, search_type=:user_name)
+        new_tweets = nil
 
-      # Deletes the first {count} elements and returns array of them (... in range)
-      @tweets_with_pictures.slice!(0...count)
-    end
+        case search_type
+        when :user_name
+          new_tweets = @client.user_timeline(
+            search_query,
+            count: MAXIMUM_SEARCH_RESULTS,
+            max_id: @last_new_tweet_id - 1
+          )
+        when :tag
+          new_tweets = @client.search(
+            search_query,
+            max_id: @last_new_tweet_id - 1
+          ).take(MAXIMUM_SEARCH_RESULTS)
+        end
+
+        new_tweets
+      end
   end
 end
